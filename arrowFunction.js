0835ecff5803789ac96dd29159239de2dcb4eaf0
Prem-Pandey <dev.premrpandey@gmail.com>
2023-11-16 02:11:13 +0530

    arr

---
+++ arrowFunction.js
@@ -0,0 +1,14 @@
+const product = (a, b) => a*b;
+
+console.log(product(5, 6))
+
+// creating object
+
+const Student = {
+    name : 'prem',
+    studentId : 12456,
+    class : '10'
+    
+
+};
+console.log(Student);
